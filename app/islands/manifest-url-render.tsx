2e79db84f9a190227cddaeb82689231714f1d9c1
Baran Wang <me@baran.wang>
2025-12-02 22:35:30 +0800

    Enhance ManifestUrlRender component with textarea interaction
    - Added a ref to the textarea for improved user interaction, allowing users to select the URL on click.
    - Updated the textarea to include a click handler that selects its content, enhancing usability.

--- app/islands/manifest-url-render.tsx
+++ app/islands/manifest-url-render.tsx
@@ -1,13 +1,20 @@
+import { useRef } from "hono/jsx";
+
 interface ManifestUrlRenderProps {
   url: string;
 }
 
 export const ManifestUrlRender = (props: ManifestUrlRenderProps) => {
+  const ref = useRef<HTMLTextAreaElement>(null);
   return (
     <textarea
+      ref={ref}
       className="field-sizing-content w-full rounded-xl border border-white/10 bg-black/30 px-4 py-3 text-center font-mono text-sm text-teal-200 focus:outline-none focus:ring-2 focus:ring-teal-500/50"
       name="manifest-url"
       readonly
+      onClick={() => {
+        ref.current?.select();
+      }}
     >
       {props.url}
     </textarea>
