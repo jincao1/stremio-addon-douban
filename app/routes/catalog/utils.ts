8e5f35fa0647548d932055324123d3b5948022e8
Jin Cao <jc.cao@live.com>
2025-12-11 07:59:28 -0500

    Update trigger frequency and prioritize IMDB id.

--- app/routes/catalog/utils.ts
+++ app/routes/catalog/utils.ts
@@ -1,11 +1,11 @@
 import type { DoubanIdMapping } from "../../db";
 
 export const generateId = (doubanId: number, params?: Partial<Omit<DoubanIdMapping, "doubanId">>) => {
-  if (params?.tmdbId) {
-    return `tmdb:${params.tmdbId}`;
-  }
   if (params?.imdbId) {
     return params.imdbId;
+  }
+  if (params?.tmdbId) {
+    return `tmdb:${params.tmdbId}`;
   }
   return `douban:${doubanId}`;
 };
